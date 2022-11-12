eb2bccb91da1e1c51792bc448c2c2c7755b62b7e
discphy <dinophy@nate.com>
2022-11-12 18:06:57 +0900

    feat(add): Order repository

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -30,4 +33,24 @@ public class OrderItem {
 
     private int count;
 
+    // ==생성 메서드==
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // ==비지니스 로직==
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    // ==조회 로직==
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
